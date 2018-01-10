5f9a703eebd026e0d82f0f74e47048327a263083
Muhammad Fajar Maulana <muhammadfajarm@gmail.com>
2018-01-10 22:38:22 +0700

    java-dao-mvc

--- src/edu/mfajarm/latihanmvcjdbc/main/LatihanCRUDMVCDAO.java
+++ src/edu/mfajarm/latihanmvcjdbc/main/LatihanCRUDMVCDAO.java
@@ -5,6 +5,13 @@
  */
 package edu.mfajarm.latihanmvcjdbc.main;
 
+import edu.mfajarm.latihanmvcjdbc.database.KingBarbershopDatabase;
+import edu.mfajarm.latihanmvcjdbc.entity.Pelanggan;
+import edu.mfajarm.latihanmvcjdbc.error.PelangganException;
+import edu.mfajarm.latihanmvcjdbc.service.PelangganDao;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Ajayfaul
@@ -13,9 +20,15 @@ public class LatihanCRUDMVCDAO {
 
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
+     * @throws edu.mfajarm.latihanmvcjdbc.error.PelangganException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, PelangganException{
         // TODO code application logic here
+        
+        
+        PelangganDao dao = KingBarbershopDatabase.getPelangganDao();
+        Pelanggan pelanggan = dao.getPelanggan("muhammadfajarm@gmail.com");
+        System.out.println("Nama "+ pelanggan.getNama());
     }
-    
 }
